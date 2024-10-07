5184eb1c890a21e89b3817e854d3c9c7c0841819
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-10-07 23:07:25 +0500

    add express app

--- index.mjs
+++ index.mjs
@@ -1,4 +1,18 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
 
+let app = express();
+app.use(express.json());
+app.use(cors());
+
+let mongoUrl = 'mongodb://localhost:27017/user_management';
+
+mongoose.connect()
+
+app.get('/users',(req,res) => {
+  res.status(200).json({message : 'GET data'});
+})
 
 
 app.listen(3000,() => {
